828f20b0a7466a072f484a38a06e894e967c5036
dj-mourabit <mohamed.mourabit@digitransform.co>
2021-02-10 10:34:35 +0100

    update translate.ts

--- generator/models.ts
+++ generator/models.ts
@@ -1,26 +1,21 @@
 export class Product {
   id = 0;
-  blockId = 0;
-  block = new Block();
-
-  tagId = 0;
-  tag = new Tag();
+  tagProducts: TagProduct[] = [];
 }
 
-export class Block {
-  id = 0;
-  name = 0;
-  shopId = 0;
-  products: Product[] = [];
+export class TagProduct {
+  tagId = 0;
+  productId = 0;
+  tag = new Tag();
+  product = new Product();
 }
 
 export class Tag {
   id = 0;
-  name = 0;
+  name = '';
   color = '';
   shopId = 0;
-  blockId = 0;
-  products: Product[] = [];
+  tagProducts: TagProduct[] = [];
 }
 
 export class Options {
