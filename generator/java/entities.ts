f501cce32b1dc79a3ed3846f65e8aa774f3cb26c
dj-mourabit <mohamed.mourabit@digitransform.co>
2021-04-20 15:32:14 +0000

    add logic for java generate

---
+++ generator/java/entities.ts
@@ -0,0 +1,103 @@
+import * as fse from 'fs-extra';
+import { HelperFunctions } from '../helper.functions';
+import { IConfigs } from '../map.helper';
+
+export class Entities {
+    constructor(private helper: HelperFunctions, private configs: IConfigs) { }
+
+
+    generateTs() {
+        const MODEL = 'model.java';
+        // else if (file === MYCONTEXT_CS) {
+        let content = fse.readFileSync(`${this.configs.pathBaseFiles}/${MODEL}`, 'utf8');
+        let models = '';
+        let seedClass = '';
+
+        fse.ensureDirSync(`${this.configs.aspFolder}/Models`);
+
+        this.configs.classes.forEach(e => {
+
+            models = `package com.sportvalue.crs.models;\r\nimport javax.persistence.*;\r\nimport com.fasterxml.jackson.annotation.JsonFormat;\r\nimport com.fasterxml.jackson.annotation.JsonIgnore;\r\n@Entity\r\n@Table(name="${this.helper.Cap(e.class)}s")\r\npublic class ${this.helper.Cap(e.class)} extends AbstractEntity {\r\n`;
+
+            e.properties.forEach(p => {
+                const isTypePrimitive = this.helper.isTypePrimitive(p.type);
+
+                if (isTypePrimitive) {
+                    if (p.name.toLowerCase() === 'id') {
+                        models += `@Id\r\n@GeneratedValue(strategy = GenerationType.IDENTITY)\r\nprivate Long ${p.name};\r\n\r\n`;
+
+                    } else if (p.name.toLowerCase() === 'email') {
+                        models += `@Column(unique = true)\r\nprivate String ${p.name}\r\n\r\n;`;
+                    } else if (p.name.toLowerCase().includes('parent')) {
+                        // modelBuilderEntity += `entity.Property(e => e.${p.name});\r\n`; // .IsRequired(false)
+                        // models += `public int? ${p.name} { get; set; }\r\n`;
+                    } else {
+                        const type = p.type === 'number' ? 'Long' : p.type === 'string' ? 'String' : p.type;
+
+                        if (type === 'Date') {
+                            models += `@Column\r\n@JsonFormat(pattern="dd/MM/yyyy'T'HH:mmZ")\r\nprivate ${type} ${p.name};\r\n\r\n`;
+                        } else {
+                            models += `@Column\r\n private ${type} ${p.name};\r\n\r\n`;
+                        }
+                    }
+                } else {
+                    if (p.type.includes('[]')) {
+
+                        if (p.name.toLowerCase() === 'childs') {
+                            // modelBuilderEntity += `entity.HasMany(e => e.${this.helper.Cap(p.name)}).WithOne(p => p.Parent).HasForeignKey(e => e.IdParent).OnDelete(DeleteBehavior.Cascade);\r\n`;
+                        } 
+
+                        const cls = p.type.replace('[]', '');
+
+                        models += `@OneToMany(cascade = CascadeType.ALL)\r\n@JoinColumn(name = "id${this.helper.Cap(e.class)}")\r\n@JsonIgnore\r\nprivate Set<${cls}> ${p.name}\r\n\r\n`;
+
+                    } else {
+                        if (p.name.toLowerCase() === 'parent') {
+                            // modelBuilderEntity += `entity.HasOne(e => e.${this.helper.Cap(p.name)}).WithMany(e => e.Childs).HasForeignKey(e => e.Id${this.helper.Cap(p.name)});\r\n`;
+                        } 
+
+                        models += `@ManyToOne(cascade = CascadeType.ALL)\r\n@JoinColumn(name = "id${this.helper.Cap(e.class)}")\r\n@JsonIgnore\r\nprivate ${this.helper.Cap(p.type !== 'any' ? p.type : p.name)} ${p.name}\r\n\r\n`;
+                    }
+                }
+            });
+
+            e.properties.forEach(p => {
+                const isTypePrimitive = this.helper.isTypePrimitive(p.type);
+
+                if (isTypePrimitive) {
+                    if (p.name.toLowerCase() === 'id') {
+                        models += `public Long getId() {\r\n\treturn id;\r\n}\r\n\r\n`;
+
+                    } else {
+                        const type = p.type === 'number' ? 'Long' : p.type === 'string' ? 'String' : p.type;
+
+                        models += `public ${type} get${this.helper.Cap(p.name)}() {\r\n\treturn ${p.name};\r\n}\r\n\r\n`;
+
+                        models += `public void set${this.helper.Cap(p.name)}(${type} ${p.name}) {\r\nthis.${p.name} = ${p.name};\r\n}\r\n\r\n`;
+                    }
+                } else {
+                    if (p.type.includes('[]')) {
+
+                    } else {
+
+                        const type = this.helper.Cap(p.type !== 'any' ? p.type : p.name);
+
+                        models += `public ${type} get${this.helper.Cap(p.name)}() {\r\n\treturn ${p.name};\r\n}\r\n\r\n`;
+
+                        models += `public void set${this.helper.Cap(p.name)}(${type} ${p.name}) {\r\nthis.${p.name} = ${p.name};\r\n}\r\n\r\n`;
+                    }
+                }
+            });
+
+            models += '}\r\n';
+
+            // for reooder things
+            // const containeForeignKey = e.properties.findIndex(o => o.name.toLowerCase() !== 'id' && o.name.startsWith('id')) !== -1;
+            // containeForeignKey ? seedClass += `.${this.helper.Cap(e.class)}s()\r\n` : seedClass = `.${this.helper.Cap(e.class)}s()\r\n` + seedClass;
+
+            fse.writeFileSync(`${this.configs.aspFolder}/models/${this.helper.Cap(e.class)}.java`, models);
+        });
+        
+        this.helper.progress(`>> ${MODEL} done`);
+    }
+}
--- generator/models.ts
+++ generator/models.ts
@@ -1,9 +1,178 @@
-export class ProductStatus {
+export class Account {
+  id = 0
+  code = '';
+  label = '';
+  formula = '';
+  type = '';
+  highlight = true;
+  bold = true;
+}
+
+
+export class AccountValue {
+  id = 0
+  value = '';
+  bspl = new Bspl();
+  account = new Account();
+}
+
+export class Bonus {
+  id = 0
+  bonus = 0;
+  comment = '';
+  type = '';
+  club = new Club();
+  noterGroup = new NoterGroup();
+}
+
+export class Bspl {
+  id = 0
+  year = 0;
+  xRate = '';
+  currency = '';
+  club = new Club();
+  bspl = new Bspl();
+}
+
+
+export class Championship {
+  id = 0
+  code = '';
+  name = '';
+  country = new Country();
+}
+
+export class Club {
+  id = 0
+  code = '';
+  name = '';
+  peerGroups = '';
+  description = '';
+  comment = '';
+  highlight = '';
+  company = '';
+  logopath = '';
+  sport = '';
+  championship = new Championship();
+}
+
+export class ClubIndicator {
+  club = new Club();
+  indicator = new Indicator();
+  score = 0;
+  type = '';
+  valueType = '';
+  value0 = '';
+  value1 = '';
+  value2 = '';
+  value3 = '';
+}
+
+export class Comment {
+  id = 0
+  comment = '';
+  type = '';
+  club = new Club();
+  noterGroup = new NoterGroup();
+}
+
+export class Country {
+  id = 0
+  code = '';
+  name = '';
+}
+
+export class CountryIndicator {
+  indicator = new Indicator();
+  country = new Country();
+  score = 0
+  value = '';
+}
+
+export class FunctionNormalizer {
+  id = 0
+  function = '';
+  indicator = new Indicator();
+}
+
+export class History {
+  id = 0
+  Date = new Date();
+  Label = '';
+  userEmail = '';
+  type = '';
+  club = new Club();
+  noterGroup = new NoterGroup();
+}
+
+export class Indicator {
+  id = 0
+  name = '';
+  weight = 0;
+  hint = '';
+  wa = true;
+  type = '';
+  sourceType = '';
+  noterGroup = new NoterGroup();
+}
+
+export class MapNormalizer {
+  id = 0
+  key = '';
+  value = 0;
+  indicator = new Indicator();
+}
+
+export class NoterGroup {
+  id = 0
+  name = '';
+  type = '';
+  weight = 0;
+  parent = new NoterGroup();
+  childs: NoterGroup[] = [];
+}
+
+export class NoterReportGroup {
+  id = 0
+  IncludeComment = true;
+  IncludeRadar = true;
+  IncludeAggregateSubthemes = true;
+  IncludeBenchmarkToMedianValue = true;
+  peerGroups = '';
+  comparedTos = '';
+  noterComments = '';
+  indicators = '';
+  reportConfig = new ReportConfig();
+}
+
+export class RangeNormalizer {
   id = 0;
-  status = '';
-  userFullName = '';
-  date = new Date();
-  userId = 0;
-  product = new Product();
-  productId = 0;
+  r0 = 0;
+  r1 = 0;
+  r2 = 0;
+  r3 = 0;
+  r4 = 0;
+  indicator = new Indicator();
+}
+
+export class ReportConfig {
+  id = 0;
+  includeRadar = false;
+  includeClubComment = false;
+  includeClubHighlight = false;
+  includeWageAndRevenue = false;
+  includeLossAndRevenue = false;
+  includeOffBalance = false;
+  includeBSPLSummary = false;
+  includeBSPLDetail = false;
+  includeBSPLHistory = false;
+}
+
+export class User {
+  id = 0;
+  email = '';
+  firstname = '';
+  lastname = '';
+  hasedpassword = '';
+  role = '';
 }
