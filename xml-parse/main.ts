f8fd6cc75b2ae51dc246a2b8e93e2d15c25f66db
djm2x <dj-m2x@hotmail.com>
2021-05-31 23:30:42 +0100

    complete crs xml export data

--- xml-parse/main.ts
+++ xml-parse/main.ts
@@ -13,7 +13,9 @@ class ClubJson {
         history: History[],
         name: string[],
         peerGroups: string[],
-        bspls: Bspl[],
+        bspls: {
+            'sv.accounting.Bspl': Bspl[]
+        }[],
         reportConfig: ReportConfig[],
         championship: { $: { reference: string } }[],
         description: string[],
@@ -28,13 +30,16 @@ class Comment {
 }
 
 class Category {
-    '$': {class: string, reference: string}
+    '$': { class: string, reference: string }
 }
 
 class Bonuse {
     entry: {
         string: string[],
-        'sv.score.indicators.Bonus': { bonus: string[] }[]
+        'sv.score.indicators.Bonus': { 
+            bonus: string[] ,
+            comment: string[] ,
+        }[]
     }[]
 }
 
@@ -52,25 +57,27 @@ class ReportConfig {
         entry: {
             string: string[],
             'sv.report.NoterReportConfig': NoterReportConfig[]
-        } []
+        }[]
     }[]
 }
 
 class NoterReportConfig {
-    includeComment :string[] = [];
-    includeRadar :string[] = [];
-    includeAggregateSuthemes :string[] = [];
-    includeBenchmarkToMedianValue :string[] = [];
-    noterComments :string[] = [];
-    indicators :string[] = [];
-    peerGroups :string[] = [];
-    comparedTo :string[] = [];
+    includeComment: string[] = [];
+    includeRadar: string[] = [];
+    includeAggregateSuthemes: string[] = [];
+    includeBenchmarkToMedianValue: string[] = [];
+    noterComments: string[] = [];
+    indicators: string[] = [];
+    peerGroups: string[] = [];
+    comparedTo: string[] = [];
 }
 
 class Bspl {
-    year: string[] = [];
-    accounts: { entry: {string: string, int: string}[] }[];
+    year: string[];
+    accounts: { entry: { string: string, int: string }[] }[];
     previous: Bspl[];
+    currency: string[];
+    xRate: string[];
 }
 
 class History {
@@ -122,7 +129,7 @@ class User {
 class value {
     entry: {
         string: string[],
-        'sv.score.indicators.Value': { 
+        'sv.score.indicators.Value': {
             value0: string,
             value1: string,
             value2: string,
@@ -145,11 +152,15 @@ async function parseXml() {
 }
 
 function writeFile(fileName: string, content: string) {
-    fse.createWriteStream(`${__dirname}/${fileName}`, { flags: 'w' }).write(content);
+    fse.createWriteStream(`${__dirname}/seeds/${fileName}`, { flags: 'w' }).write(content);
 }
 
 function h(e: string[]): string {
-    return e ? e[0].replace(/\"/g, '\'').replace(/\n/g, '') : null;
+    try {
+        return e ? e[0]?.replace(/\"/g, '\'').replace(/\n/g, '') : null;
+    } catch (error) {
+        return null;
+    }
 }
 
 function clubChampionShip(e: { $: { reference: string } }[]): string {
@@ -183,65 +194,110 @@ async function parseClub() {
     const file = fse.readFileSync(`${__dirname}/clubs.json`);
 
     const r = JSON.parse(file as any) as ClubJson[];
-    let clubsString = '';
+    let bonusString = '';
     let reportConfigString = '';
     let NoterReportConfigString = '';
     let commentString = '';
-
     let clubIndicatorString = '';
     let IndicatorString = '';
     let bsplString = '';
+    let accountValueString = '';
 
+    // ids
+    let bonusId = 0;
     let clubIndicatorId = 0;
     let IndicatorId = 0;
     let bsplId = 0;
+    let accountValueId = 0;
     let reportConfigId = 0;
     let commentId = 0;
     let NoterReportConfigId = 0;
     r.forEach((e, i) => {
         const o = e['sv.entities.Club'][0];
+        const clubId = i + 1;
+        // clubsString += `new Club(${clubId}L, "${h(o.code)}", "${h(o.name)}", "${h(o.description)}", "", "${h(o.highlight)}", "${h(o.company)}", "${h(o.logoPath)}", ${clubChampionShip(o.championship)}, 1L),\r\n`;
 
-        // clubsString += `new Club(${i + 1}L, "${h(o.code)}", "${h(o.name)}", "${h(o.description)}", "", "${h(o.highlight)}", "${h(o.company)}", "${h(o.logoPath)}", ${clubChampionShip(o.championship)}, 1L),\r\n`;
-    
         o.reportConfig.forEach((r, j) => {
-            reportConfigId = j + 1;
-            reportConfigString += `new ReportConfig(${reportConfigId}L, ${h(r.includeRadar)}, ${h(r.includeClubComment)}, ${h(r.includeClubHighlight)}, ${h(r.includeWageAndRevenue)}, ${h(r.includeLossAndRevenue)}, ${h(r.includeOffBalance)}, ${h(r.includeBSPLSummary)}, ${h(r.includeBSPLDetail)}, ${h(r.includeBSPLHistory)}, ${i + 1}L),`
-        
-            
-            const NoterReportConfig0: NoterReportConfig[] = [].concat(...r.themeConfigs.map(e => e.entry.map(x => x['sv.report.NoterReportConfig'])));
-            const NoterReportConfig = r.themeConfigs[0]?.entry.map(x => x['sv.report.NoterReportConfig'][0]);
+            reportConfigId = j + 1 + i;
+            reportConfigString += `new ReportConfig(${reportConfigId}L, ${h(r.includeRadar)}, ${h(r.includeClubComment)}, ${h(r.includeClubHighlight)}, ${h(r.includeWageAndRevenue)}, ${h(r.includeLossAndRevenue)}, ${h(r.includeOffBalance)}, ${h(r.includeBSPLSummary)}, ${h(r.includeBSPLDetail)}, ${h(r.includeBSPLHistory)}, ${clubId}L),\r\n`
 
-            NoterReportConfig.forEach((t,k) => {
+
+            // const NoterReportConfig0: NoterReportConfig[] = [].concat(...r.themeConfigs.map(e => e.entry.map(x => x['sv.report.NoterReportConfig'])));
+            const NoterReportConfig = r.themeConfigs[0]?.entry?.map(x => x['sv.report.NoterReportConfig'][0]);
+
+            NoterReportConfig?.forEach((t, k) => {
                 NoterReportConfigId = k + 1;
 
-                NoterReportConfigString += `new NoterReportGroup(${NoterReportConfigId}L, ${h(t.includeComment)}, ${h(t.includeRadar)}, false, ${h(t.includeBenchmarkToMedianValue)}, "${h(t.peerGroups)}", "${h(t.comparedTo)}", "${h(t.noterComments)}", "club", "${h(t.indicators)}", ${reportConfigId}L),`
+                NoterReportConfigString += `new NoterReportGroup(${NoterReportConfigId}L, ${h(t.includeComment)}, ${h(t.includeRadar)}, false, ${h(t.includeBenchmarkToMedianValue)}, "${h(t.peerGroups)}", "${h(t.comparedTo)}", "${h(t.noterComments)}", "club", "${h(t.indicators)}", ${reportConfigId}L),\r\n`
             });
         });
 
-        o.comments.map(c => c.entry[0]).forEach((c, ci) => {
-            commentId = ci + 1;
-            commentString += `new Comment(${commentId}L, "${h([c.string[1]])}", "club", ${i + 1}L, null),`
+        o.comments.map(c => c.entry ? c.entry[0] : [] as any).forEach((c, ci) => {
+            commentId = ci + clubId;
+            commentString += `new Comment(${commentId}L, "${h([c.string ? c.string[1] : []])}", "club", ${clubId}L, null),\r\n`
+        });
+
+        o.bonuses[0].entry?.forEach((c, ci) => {
+            //  bonusId = ci + clubId;
+            const ob = c['sv.score.indicators.Bonus'][0]
+            bonusString += `new Bonus(${++bonusId}L, ${h(ob?.bonus)}f, "${h(ob?.comment)}", "${h(c.string)}", ${clubId}L),\r\n`
         });
 
 
-        o.values.map(c => c.entry[0]).forEach((c, ci) => {
-            IndicatorId = ci + 1;
-            IndicatorString += `new Indicator(${IndicatorId}L, "${h(c.string)}", 4L, "${h(c.string)}", false, null, "club", null),`;
+        o.values.map(c => c.entry ? c.entry[0] : [] as any).forEach((c, ci) => {
+            IndicatorId = ci + clubId;
+            IndicatorString += `new Indicator(${IndicatorId}L, "${h(c.string)}", 4L, "${h(c.string)}", false, null, "club", null),\r\n`;
 
-            c['sv.score.indicators.Value'].forEach((cl, cli) => {
-                clubIndicatorId = cli + 1;
+            c['sv.score.indicators.Value']?.forEach((cl, cli) => {
+                clubIndicatorId = cli + clubId;
 
-                clubIndicatorString += `new ClubIndicator(${clubIndicatorId}L, 0, null, null, ${cl.value0}L, ${cl.value1}L, ${cl.value2}L, ${cl.value3}L, ${i + 1}L, ${IndicatorId}L),`;
+                clubIndicatorString += `new ClubIndicator(${clubIndicatorId}L, 0L, null, null, "${cl.value0 || '0'}", "${cl.value1 || '0'}", "${cl.value2 || '0'}", "${cl.value3 || '0'}", ${clubId}L, ${IndicatorId}L),\r\n`;
             })
         });
 
-        o.bspls.map(c => c.entry[0]).forEach((c, ci) => {
-            commentId = ci + 1;
-            commentString += `new Comment(${commentId}L, "${h([c.string[1]])}", "club", ${i + 1}L, null),`
-        });
+        let bspls: Bspl[] = [];
+
+        try {
+            bspls = o.bspls[0]['sv.accounting.Bspl']
+        } catch { }
+
+        // bsplId = clubId;
+
+
+        (function fn(bs: Bspl[]) {
+            const b = bs ? bs[0] : null;
+
+            if (!b) {
+                return;
+            }
+
+            
+            bsplString += `new Bspl(${++bsplId}L, "", "", "", "", false, false, ${b.xRate}f, "${b.currency}", ${bsplId > 1 ? bsplId - 1 + 'L' : null}),\r\n`;
+
+            b.accounts[0].entry?.forEach((ac, acIndex) => {
+                accountValueId += 1;
+                accountValueString += `INSERT INTO account_values (value, year , id_club, id_bspl) values ('${ac.int}', ${h(b.year)}, ${clubId}, ${bsplId});\r\n`;
+            })
+
+
+            if (b) {
+                fn(b.previous);
+            }
+        })(bspls);
+
+
+
     });
 
-    writeFile(`clusSeed.txt`, clubsString);
+    writeFile(`reportConfigString.txt`, reportConfigString);
+    writeFile(`NoterReportConfigString.txt`, NoterReportConfigString);
+    writeFile(`commentString.txt`, commentString);
+    writeFile(`clubIndicatorString.txt`, clubIndicatorString);
+    writeFile(`bsplString.txt`, bsplString);
+    writeFile(`accountValueString.sql`, accountValueString);
+    writeFile(`IndicatorString.txt`, IndicatorString);
+    writeFile(`clubIndicatorString.txt`, clubIndicatorString);
+    writeFile(`bonusString.txt`, bonusString);
 }
 
 async function parseCountry() {
